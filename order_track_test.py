7def6547ac4007a4178b2af912706aecc268b627
Natalia-Litovka <153516082+Natalia-Litovka@users.noreply.github.com>
2023-12-12 13:25:24 +0300

    Update order_track_test.py

--- order_track_test.py
+++ order_track_test.py
@@ -3,6 +3,5 @@ import sender_stand_request
 
 
 def test_create_and_track_order():
-    track_number = sender_stand_request.get_track_number_order()
-    get_response = sender_stand_request.get_order_info(track_number)
+    get_response = sender_stand_request.get_order_info()
     assert get_response.status_code == 200
