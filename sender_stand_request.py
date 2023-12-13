47a87acc83e220682986851dcda64c48f2bba317
Natalia-Litovka <153516082+Natalia-Litovka@users.noreply.github.com>
2023-12-13 11:29:40 +0300

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -9,6 +9,6 @@ def post_new_order(order_body):
                          headers=data.headers)
 
 
-def get_order_info(track_number):
+def get_order_info(track):
     return requests.get(configuration.URL_SERVICE + configuration.ORDER_TRACK,
-                        params={"t": track_number})
+                        params={"t": track})
