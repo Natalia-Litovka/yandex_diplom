37cc13a07da86797ae937366af9d42aef8d9a847
Natalia-Litovka <153516082+Natalia-Litovka@users.noreply.github.com>
2023-12-13 11:06:55 +0300

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -3,14 +3,12 @@ import configuration
 import data
 
 
-def post_new_order():
+def post_new_order(order_body):
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER,
                          json=data.order_body,
                          headers=data.headers)
 
 
-def get_order_info():
-    track_number = data.headers.copy()
-    track_number["track"] = "t"
+def get_order_info(track_number):
     return requests.get(configuration.URL_SERVICE + configuration.ORDER_TRACK,
                         params={"t": track_number})
