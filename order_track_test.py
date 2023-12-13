83f540ec2e448f239d21cbcc6d1f74d53f306c67
Natalia-Litovka <153516082+Natalia-Litovka@users.noreply.github.com>
2023-12-13 11:07:11 +0300

    Update order_track_test.py

--- order_track_test.py
+++ order_track_test.py
@@ -1,7 +1,27 @@
 # Наталья Литовка, 11-я когорта - Финальный проект. Инженер по тестированию плюс
 import sender_stand_request
+import data
 
 
-def test_create_and_track_order():
-    get_response = sender_stand_request.get_order_info()
-    assert get_response.status_code == 200
+def get_new_order_track():
+    # Создать новый заказ
+    order_body = data.order_body
+    # получить ответ по созданному заказу
+    response_order = sender_stand_request.post_new_order(order_body)
+    # Запомнить трек заказа
+    return response_order.json()
+
+
+# получить данные заказа по треку
+def get_order_info():
+    # сохраним трек заказа
+    track_number = get_new_order_track()
+    # запрос на данные по номеру трека
+    response_order = sender_stand_request.get_order_info(track_number)
+    # вернуть ответ сервера
+    return response_order
+
+
+# проверить статус ответа
+def test_order_info():
+    assert get_order_info().status_code == 200
